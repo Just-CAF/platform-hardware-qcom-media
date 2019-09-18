c2bccd7c2a87280ca4d515893d487f31616dc1d8
T1mpp4 <timi.rautamaki@gmail.com>
2019-09-18 08:59:49 +0200

    if check not working -> disable for now
    Change-Id: I2902496ed795aa4341b86a470e7f54b4f16b34a7

--- Android.mk
+++ Android.mk
@@ -1,11 +1,11 @@
 QCOM_MEDIA_ROOT := $(call my-dir)
 
 #Compile these for all targets under QCOM_BOARD_PLATFORMS list.
-ifeq ($(call is-board-platform-in-list, $(QCOM_BOARD_PLATFORMS)),true)
+#ifeq ($(call is-board-platform-in-list, $(QCOM_BOARD_PLATFORMS)),true)
 include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
 include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
 include $(QCOM_MEDIA_ROOT)/libaac/Android.mk
-endif
+#endif
 
 ifeq ($(call is-board-platform-in-list, $(MSM_VIDC_TARGET_LIST)),true)
 include $(QCOM_MEDIA_ROOT)/mm-video-v4l2/Android.mk
